9c55c64087dfc3aa343f64ee9195865840643bab
pedro <pedro@kali.home>
2022-01-27 10:57:44 +0100

    general

--- publicip.py
+++ publicip.py
@@ -3,12 +3,5 @@ import json
 import requests
 r = requests.get('https://ipinfo.io/')
 js = json.loads(r.text)
-print(f"Public IP -> {js['ip']}\n"
-      f"HostName -> {js['hostname']}\n"
-      f"Country, Region, City -> {js['country']}{js['region']}{js['city']}\n"
-      f"Location -> {js['loc']}\n"
-      f"Organization -> {js['org']}\n"
-      f"Postal -> {js['postal']}\n"
-      f"Timezone -> {js['timezone']}")
-
+print(js)
 
